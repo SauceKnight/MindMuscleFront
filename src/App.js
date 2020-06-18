c90e4e234ef7035e868d572be2f8c07886356222
Alfredo <aquirogatech@gmail.com>
2020-06-18 19:25:01 -0400

    created react app and implemented model viewer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Link, useHistory } from 'react-router-dom'
+import TopNav from './components/TopNav'
+import Drawer from '@material-ui/core/Drawer';
+import { makeStyles } from '@material-ui/core/styles';
+import { Toolbar } from '@material-ui/core';
+
+const drawerWidth = "50%"
+
+const useStyles = makeStyles((theme) => ({
+  drawer: {
+    flexShrink: 0,
+    width: drawerWidth
+  },
+  root: {
+    display: 'flex',
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  model: {
+    width: "100%",
+    height: "100%"
+  }
+}));
 
 function App() {
+
+  const modelViewer = document.querySelector('#model-demo');
+  const history = useHistory();
+  const classes = useStyles();
+  // const orbitCycle = [
+  //   "0deg 60deg -2m",
+  //   modelViewer.cameraOrbit
+  // ];
+
+  const handleClick = (modelViewer) => {
+    history.push('/Chest')
+
+    // modelViewer.setFieldOfView("0deg 0deg")
+  }
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <TopNav />
+      <Drawer className={classes.drawer}
+        variant="permanent" classes={{
+          paper: classes.drawerPaper,
+        }}>
+        <Toolbar />
+        <model-viewer id="model-demo" src="/3dModel/HumanModel.glb"
+          camera-controls
+          alt="A 3D model of an astronaut" style={{ width: 100 + '%', height: 100 + '%' }}>
+          <button slot="hotspot-chest" data-position="-0.38579194181026033 7.483364059519481 0.7423148470799796" data-normal="0 0 1" data-visibility-attribute="visible" onClick={handleClick}></button>
+
+        </model-viewer>
+
+      </Drawer>
+      <main className={classes.content}>
+        <Toolbar />
+        <p>Hello</p>
+
+      </main>
     </div>
   );
 }
