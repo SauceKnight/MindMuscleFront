f7c9922e9af9f2525d1ac61b16072a11f55722a1
Alfredo <aquirogatech@gmail.com>
2020-06-25 12:52:35 -0400

    Corrected API value for deployment

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    API: process.env.NODE_ENV === "development" ? 'http://localhost:5000' : 'https://mindmusclebackend.herokuapp.com/'
+    API: process.env.NODE_ENV === "development" ? 'http://localhost:5000' : 'https://mindmusclebackend.herokuapp.com'
 }
